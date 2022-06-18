25418de5932cfc13c21a0b34533c320f696650f8
Mikey Lau <mikey.lau@traininblocks.com>
2022-06-18 21:53:00 +0100

    doc: Basic example class and method documentation

--- examples/basic/__main__.py
+++ examples/basic/__main__.py
@@ -3,6 +3,8 @@
 As per instructions from https://github.com/Lightning-AI/lightning.
 """
 
+# pylint: disable=invalid-name, arguments-differ, too-many-ancestors
+
 from torch.optim import Adam
 from torch.nn.functional import mse_loss
 from torch.nn import Sequential, Linear, ReLU
@@ -14,35 +16,68 @@ from pytorch_lightning.loggers import TensorBoardLogger
 
 
 class LitAutoEncoder(LightningModule):
+    """Lighning module to handle ML steps."""
+
     def __init__(self):
+        """Creates ML model."""
+
         super().__init__()
         self.encoder = Sequential(Linear(28 * 28, 128), ReLU(), Linear(128, 3))
         self.decoder = Sequential(Linear(3, 128), ReLU(), Linear(128, 28 * 28))
 
-    def forward(self, x):
-        # in lightning, forward defines the prediction/inference actions
-        embedding = self.encoder(x)
-        return embedding
+    def configure_optimizers(self):
+        """Gradient descent optimizer for forward pass and backpropagation.
+
+        Returns:
+            optimizer: The model's optimizer.
+        """
+
+        optimizer = Adam(self.parameters(), lr=1e-3)
+        return optimizer
 
     def training_step(self, batch):
-        # training_step defines the train loop. It is independent of forward
-        x, y = batch
+        """Trains the ML model.
+
+        Args:
+            batch: the batch size.
+
+        Returns:
+            loss: The loss of the model.
+        """
+
+        x, _ = batch
         x = x.view(x.size(0), -1)
         z = self.encoder(x)
         x_hat = self.decoder(z)
         loss = mse_loss(x_hat, x)
         self.log("train_loss", loss)
+
         return loss
 
-    def configure_optimizers(self):
-        optimizer = Adam(self.parameters(), lr=1e-3)
-        return optimizer
+    def forward(self, x):
+        """Inference and prediction method.
+
+        Args:
+            x: Value used for inference or prediction.
+
+        Returns:
+            embedding: The infered/predicted value(s).
+        """
+
+        embedding = self.encoder(x)
+        return embedding
 
 
+# Gets and pre-processes data
 dataset = MNIST("data", download=True, transform=transforms.ToTensor())
 train, val = random_split(dataset, [55000, 5000])
 
+# Instantiates model
 autoencoder = LitAutoEncoder()
+
+# Custom logger
 tb_logger = TensorBoardLogger(save_dir="examples/basic/logs/")
 trainer = Trainer(logger=tb_logger)
+
+# Initiates training
 trainer.fit(autoencoder, DataLoader(train), DataLoader(val))
